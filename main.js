3ffc7b72f3f41f219f0b5ddf20aa0ec3dab13971
namborghini <121301710+n4mborgh1n1@users.noreply.github.com>
2023-03-02 20:31:08 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,28 @@
-const input = document.getElementById('inputBill');
-input.addEventListener('input', () => {
-  input.value = input.value.replace(/[^0-9.]/g, '');
+const inputBill = document.getElementById('inputBill');
+inputBill.addEventListener('input', () => {
+  let billValue = inputBill.value;
+
+  // begrenze auf zwei Nachkommastellen nach einem Punkt
+  if (billValue.includes('.')) {
+    const [integerPart, decimalPart] = billValue.split('.');
+    billValue = `${integerPart.slice(0, 7)}.${decimalPart.slice(0, 2)}`;
+  } else {
+    billValue = billValue.slice(0, 7);
+  }
+
+  // entferne alle Zeichen außer Zahlen und Punkt
+  inputBill.value = billValue.replace(/[^0-9.]/g, '');
 });
 
 const inputPeople = document.getElementById('inputPeople');
 inputPeople.addEventListener('input', () => {
-  inputPeople.value = inputPeople.value.replace(/[^0-9]/g, '');
+  let peopleValue = inputPeople.value;
+
+  // begrenze auf maximal 500 Personen
+  if (peopleValue > 500) {
+    peopleValue = '500';
+  }
+  
+  // entferne alle Zeichen außer Zahlen
+  inputPeople.value = peopleValue.replace(/[^0-9]/g, '');
 });
