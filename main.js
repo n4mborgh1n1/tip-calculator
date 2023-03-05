345c72e08a4b5f889360e264667a4d405a1989ef
namborghini <121301710+n4mborgh1n1@users.noreply.github.com>
2023-03-05 05:25:12 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,22 +2,25 @@ const inputBill = document.getElementById('inputBill');
 const inputPeople = document.getElementById('inputPeople');
 const tipContainer = document.getElementById('tipContainer');
 const tipPercentages = [5, 10, 15, 25, 50];
-
 const totalPerPersonElement = document.getElementById('totalPerPerson');
 const totalElement = document.getElementById('total');
 
 // initialize tip percentage to 0%
 let tipPercentage = 0;
+let customTipPercentage = 0;
 
 // add event listener to the tip percentage radio buttons
 tipContainer.addEventListener('click', function(event) {
   if (event.target.value !== 'custom') {
     tipPercentage = parseInt(event.target.value);
+    customTipPercentage = 0;
+    tipInput.value = '';
   } else {
     tipPercentage = 0;
   }
   calculateTip();
 });
+
 
 // add event listener to the bill and number of people input fields
 inputBill.addEventListener('input', calculateTip);
@@ -38,15 +41,15 @@ function calculateTip() {
   const numberOfPeople = parseInt(inputPeople.value);
 
   // calculate the tip amount and the total amount per person
-  let tipAmount = (billAmount * tipPercentage / 100) / numberOfPeople;
+  let tipAmount = (billAmount * (tipPercentage + customTipPercentage) / 100) / numberOfPeople;
   let totalPerPerson = (billAmount + tipAmount) / numberOfPeople;
   
   if (numberOfPeople === 1) {
-    tipAmount = billAmount * (tipPercentage / 100);
+    tipAmount = billAmount * ((tipPercentage + customTipPercentage) / 100);
     totalPerPerson = billAmount + tipAmount;
   } else {
-    tipAmount = (billAmount * tipPercentage / 100) / numberOfPeople;
-    totalPerPerson = (billAmount + tipAmount * 2) / numberOfPeople;
+    tipAmount = (billAmount * (tipPercentage + customTipPercentage) / 100) / numberOfPeople;
+    totalPerPerson = (billAmount + tipAmount * numberOfPeople) / numberOfPeople;
   }
   
   // display the tip amount and total per person
@@ -59,6 +62,7 @@ function calculateTip() {
   }
 }
 
+
 // input field restriction
 inputBill.addEventListener("input", () => {
   let billValue = inputBill.value;
@@ -66,9 +70,9 @@ inputBill.addEventListener("input", () => {
   // decimal part restriction -> auf 2 kommastellen
   if (billValue.includes(".")) {
     const [integerPart, decimalPart] = billValue.split(".");
-    billValue = `${integerPart.slice(0, 7)}.${decimalPart.slice(0, 2)}`;
+    billValue = `${integerPart.slice(0, 4)}.${decimalPart.slice(0, 2)}`;
   } else {
-    billValue = billValue.slice(0, 7);
+    billValue = billValue.slice(0, 4);
   }
 
   // only interger and point
@@ -92,3 +96,57 @@ inputPeople.addEventListener("input", () => {
 
   calculateTip();
 });
+
+const tipInput = document.querySelector('.tipCos');
+
+tipInput.addEventListener('click', function() {
+  tipInput.placeholder = '';
+
+});
+
+tipInput.addEventListener('input', function() {
+  let tipValue = tipInput.value;
+
+  tipInput.addEventListener('input', function() {
+    let tipValue = tipInput.value;
+  
+    // restrict to 2 digits and allow only numbers
+    if (tipValue.length > 3) {
+      tipValue = tipValue.slice(0, 3);
+    }
+    tipValue = tipValue.replace(/[^0-9]/g, "");
+    tipInput.value = tipValue.length > 0 ? `${tipValue}%` : '';
+  
+    // set customTipPercentage to the value of the input field
+    customTipPercentage = parseInt(tipValue);
+    
+    // uncheck the other tip percentage radio buttons
+    const tipRadioButtons = document.querySelectorAll('.tip');
+    tipRadioButtons.forEach(function(button) {
+      if (button.value !== 'custom') {
+        button.checked = false;
+      }
+    });
+  
+    calculateTip();
+  });
+  
+  // set customTipPercentage to the value of the input field
+  customTipPercentage = parseInt(tipInput.value);
+  
+  // uncheck the other tip percentage radio buttons
+  const tipRadioButtons = document.querySelectorAll('.tip');
+  tipRadioButtons.forEach(function(button) {
+    if (button.value !== 'custom') {
+      button.checked = false;
+    }
+  });
+
+  calculateTip();
+});
+
+
+
+
+
+
